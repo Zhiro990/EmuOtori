c422142d434c7a9533374acf97ddcfa89bdf9433
Zhiro <66927402+Zhiro990@users.noreply.github.com>
2024-03-13 16:31:16 +0000

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Layout from "$lib/+layout.svelte";
+	import Footer from "$lib/+footer.svelte";
+
+	import { page } from "$app/stores";
+</script>
+
+<Layout>
+	<div class="w-full h-auto mt-[75px]">
+		<div
+			class="w-fit h-auto bg-[#ffffff] px-[20px] py-[5px] mx-auto rounded-lg"
+		>
+			<p class="font-bold text-[18px] text-[#5a6dfa]">Error: {$page.status}</p>
+		</div>
+		<p class="font-bold text-[18px] text-[#ffffff] mt-[30px]">
+			{$page.error.message}
+		</p>
+	</div>
+
+	<Footer />
+</Layout>
