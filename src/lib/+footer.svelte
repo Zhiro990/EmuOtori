371edfc8974341c4f6ccd21093f7c3a925ac74dd
Zhiro <66927402+Zhiro990@users.noreply.github.com>
2024-12-20 12:33:45 +0000

    Added 2 files and edited 2 files.

--- src/lib/+footer.svelte
+++ src/lib/+footer.svelte
@@ -1,3 +1,3 @@
-<div class="w-full h-auto mt-[25px]">
-	<p class="font-bold text-[17px] text-[#ffffff]">Created by Zecotron</p>
+<div class="w-full h-auto mt-[25px] select-none">
+	<p class="font-bold text-[17px] text-[#ffffff]">© Zecotron 2024</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,14 @@
 	import Footer from "$lib/+footer.svelte";
 
 	let clicks = 0;
+	let playaudio = false;
 
 	async function SummonEmu(trigger, event) {
 		if (trigger == "click" && event.pointerType != "mouse") return;
 
 		let body = document.querySelector("body");
 		let score = document.getElementById("score");
+
 		let img = document.createElement("img");
 		let audio = document.createElement("audio");
 
@@ -26,7 +28,7 @@
 		img.alt = "Emu Otori";
 
 		audio.src = "./audios/emuotori.mp3";
-		audio.volume = 0.4;
+		audio.volume = playaudio ? 0.4 : 0;
 
 		score.innerHTML = clicks;
 
@@ -45,6 +47,14 @@
 			body.removeChild(audio);
 		}, 3000);
 	}
+
+	function ChangeAudioSetting() {
+		let audiosettingbuttonimg = document.getElementById("audio-setting-button-img");
+
+		audiosettingbuttonimg.src = "./pictures/audio-" + (playaudio ? "off" : "on") + ".svg";
+
+		playaudio = !playaudio;
+	}
 </script>
 
 <Layout>
@@ -54,15 +64,39 @@
 		on:touchstart={event => SummonEmu("touch", event)}
 	>
 		<div
-			class="w-fit h-auto bg-[#ffffff] px-[20px] py-[5px] mt-[75px] mx-auto rounded-lg"
+			class="w-fit h-auto bg-[#ffffff] px-[20px] py-[5px] mt-[75px] mx-auto rounded-lg select-none"
 		>
 			<p class="font-bold text-[21px] text-[#5a6dfa]">EmuOtori</p>
 		</div>
 
-		<div class="w-full h-auto mb-[325px]">
+		<div class="w-full h-auto mb-[390px] md:mb-[330px] select-none">
 			<p id="score" class="text-[30px] text-[#ffffff] mt-[25px]">0</p>
 		</div>
 
 		<Footer />
+
+		<div
+			id="audio-setting"
+			class="w-full md:w-fit h-fit flex mt-[12.5px] md:mt-0 items-center space-x-[15px] md:top-[20px] right-[25px] absolute"
+		>
+			<input
+				type="range"
+				id=""
+				class=""
+			/>
+
+			<button
+				id="audio-setting-button"
+				class="size-fit"
+				on:click={ChangeAudioSetting}
+			>
+				<img
+					id="audio-setting-button-img"
+					src="./pictures/audio-off.svg"
+					class="size-[35px] md:size-[40px] select-none"
+					alt="Audio Setting"
+				/>
+			</button>
+		</div>
 	</div>
 </Layout>
